3f8a9a181355cf4a578f4691a85b557df903d0f7
Rohan Almeida <rohan@edyst.com>
2021-07-16 21:25:15 +0530

    go fmt

--- cpuse.go
+++ cpuse.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "log"
-    "strings"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 
-    "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    // Get host
-    fmt.Print("Enter remote host: ")
-    remoteHostName, err := reader.ReadString('\n')
-    if err != nil {
-        log.Fatal("Failed to read host: ", err)
-    }
-    remoteHostName = strings.TrimRight(remoteHostName, "\n")
-    
-    remotePort := 22
-    remoteHost := fmt.Sprintf("%s:%d", remoteHostName, remotePort)
+	// Get host
+	fmt.Print("Enter remote host: ")
+	remoteHostName, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Failed to read host: ", err)
+	}
+	remoteHostName = strings.TrimRight(remoteHostName, "\n")
 
-    fmt.Print("Enter username: ")
-    userName, err := reader.ReadString('\n')
-    if err != nil {
-        log.Fatal("Failed to read username: ", err)
-    }
-    userName = strings.TrimRight(userName, "\n")
+	remotePort := 22
+	remoteHost := fmt.Sprintf("%s:%d", remoteHostName, remotePort)
 
-    // use private key
-    fmt.Print("Enter path to private key: ")
-    keyFile, err := reader.ReadString('\n')
-    if err != nil {
-        log.Fatal("Failed to read private key path: ", err)
-    }
-    keyFile = strings.TrimRight(keyFile, "\n")
+	fmt.Print("Enter username: ")
+	userName, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Failed to read username: ", err)
+	}
+	userName = strings.TrimRight(userName, "\n")
 
-    key, err := ioutil.ReadFile(keyFile)
+	// use private key
+	fmt.Print("Enter path to private key: ")
+	keyFile, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Failed to read private key path: ", err)
+	}
+	keyFile = strings.TrimRight(keyFile, "\n")
+
+	key, err := ioutil.ReadFile(keyFile)
 	if err != nil {
 		log.Fatalf("Unable to read private key: %v", err)
 	}
@@ -50,20 +50,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to parse private key: %v", err)
 	}
-    
-    // a config shared between client and server
-    config := &ssh.ClientConfig{
-        User: userName,
-        Auth: []ssh.AuthMethod{
-            ssh.PublicKeys(signer),
-        },
-        HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-    }
 
-    log.Printf("Connecting to %s...\n", remoteHost)
-    client, err := ssh.Dial("tcp", remoteHost, config)
-    if err != nil {
-        log.Fatal("Failed to dial: ", err)
-    }
-    defer client.Close()
+	// a config shared between client and server
+	config := &ssh.ClientConfig{
+		User: userName,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	log.Printf("Connecting to %s...\n", remoteHost)
+	client, err := ssh.Dial("tcp", remoteHost, config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer client.Close()
 }
