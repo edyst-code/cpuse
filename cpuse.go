ea947b2b7270c20c753796b5df9e85c07521d2cb
Rohan Almeida <rohan@edyst.com>
2021-07-16 20:52:08 +0530

    read host, user, password from stdin

--- cpuse.go
+++ cpuse.go
@@ -1,9 +1,58 @@
 package main
 
 import (
-  "fmt"
+    "bufio"
+    "fmt"
+    "os"
+    "log"
+    "strings"
+    "syscall"
+
+    "golang.org/x/crypto/ssh"
+    "golang.org/x/term"
 )
 
 func main() {
-  fmt.Println("Hello from cpuse")
+    reader := bufio.NewReader(os.Stdin)
+
+    // Get host
+    fmt.Print("Enter remote host: ")
+    remoteHostName, err := reader.ReadString('\n')
+    if err != nil {
+        log.Fatal("Failed to read host: ", err)
+    }
+    remoteHostName = strings.TrimRight(remoteHostName, "\n")
+    
+    remotePort := 22
+    remoteHost := fmt.Sprintf("%s:%d", remoteHostName, remotePort)
+
+    fmt.Print("Enter username: ")
+    userName, err := reader.ReadString('\n')
+    if err != nil {
+        log.Fatal("Failed to read username: ", err)
+    }
+    userName = strings.TrimRight(userName, "\n")
+
+    fmt.Print("Enter password: ")
+    bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+    if err != nil {
+        log.Fatal("Failed to read password: ", err)
+    }
+    password := string(bytePassword)
+
+    // a config shared between client and server
+    config := &ssh.ClientConfig{
+        User: userName,
+        Auth: []ssh.AuthMethod{
+            ssh.Password(password),
+        },
+        HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+    }
+
+    log.Printf("Connecting to %s...\n", remoteHost)
+    client, err := ssh.Dial("tcp", remoteHost, config)
+    if err != nil {
+        log.Fatal("Failed to dial: ", err)
+    }
+    defer client.Close()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/edyst-code/cpuse
 
 go 1.16
+
+require (
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
+)
